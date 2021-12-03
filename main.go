5b7a61e3f7b7781a3f170344cb743a5eae249e26
Yannick Musafiri <musayann@octangrp.com>
2021-12-03 18:44:32 +0200

    feat: add v1 apis (#1)

--- main.go
+++ main.go
@@ -10,11 +10,14 @@ func main() {
 	Router = gin.Default()
 	api := Router.Group("/api")
 	{
-		api.GET("/test", func(ctx *gin.Context) {
-			ctx.JSON(200, gin.H{
-				"message": "test successful",
+		v1 := api.Group("/v1")
+		{
+			v1.GET("/test", func(ctx *gin.Context) {
+				ctx.JSON(200, gin.H{
+					"message": "test successful",
+				})
 			})
-		})
+		}
 	}
 	Router.Run(":3080")
 }
